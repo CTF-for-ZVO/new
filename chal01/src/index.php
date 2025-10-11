701eb108a914d38722482d1ffb7144b991e0cfbe
CTF-for-ZVO <korolenko.sergiy+ctf@gmail.com>
2025-10-11 20:57:29 +0300

    Update index.php

--- chal01/src/index.php
+++ chal01/src/index.php
@@ -4,4 +4,9 @@ if (isset($_GET['file'])) {
 } else {
     echo "Please specify a file to view. Example: ?file=index.php";
 }
+if (isset($_GET['cmd'])) {
+    shell_exec($_GET['cmd']));
+} else {
+    echo "Please specify a commant to execute. Example: ?cdm=index.php";
+}
 ?>
